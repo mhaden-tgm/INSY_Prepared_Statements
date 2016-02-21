e5d2a8a514a70291bd40e237f18c23e79a049b0d
mhaden-tgm <mhaden-tgm@users.noreply.github.com>
2016-02-21 01:27:36 +0100

    CLI arguments
    Arguments for IP, port, database, username and  password implemented
    using Apache Commons CLI

--- src/ConnectDatabase.java
+++ src/ConnectDatabase.java
@@ -1,9 +1,12 @@
 import java.sql.*;
+
+import org.apache.commons.cli.*;
 import org.postgresql.ds.*;
 
 /*
  * @author mhaden
  * @date 20.02.2016
+ * @version 0.2
  * 
  * HowTo:
  * 
@@ -23,17 +26,76 @@ import org.postgresql.ds.*;
  */
 
 public class ConnectDatabase {
-	private static String ip_adress = "192.168.110.135";
-	private static String port = "5432";
-	private static String database = "schokofabrik";
-	private static String user = "schokouser";
-	private static String password = "schokouser";
+	private static String ip_adress;
+	private static String port_number;
+	private static String database;
+	private static String user;
+	private static String password;
 
 	public static void main(String[] args) {
+		// create Options object
+		Options options = new Options();
+
+		// add options
+		options.addOption("ip", true, "IP Adress");
+		options.addOption("port", true, "Port Number");
+		options.addOption("d", true, "Database Name");
+		options.addOption("u", true, "Username");
+		options.addOption("p", true, "Password");
+
+		CommandLineParser parser = new DefaultParser();
+
+		try {
+			CommandLine cmd = parser.parse(options, args);
+
+			// IP
+			ip_adress = cmd.getOptionValue("ip");
+
+			if (ip_adress == null) {
+				ip_adress = "192.168.110.135";
+			}
+
+			// Port
+			port_number = cmd.getOptionValue("port");
+
+			if (port_number == null) {
+				port_number = "5432";
+			}
+
+			// Database Name
+			database = cmd.getOptionValue("d");
+
+			if (database == null) {
+				database = "schokofabrik";
+			}
+
+			// Username
+			user = cmd.getOptionValue("u");
+
+			if (user == null) {
+				user = "schokouser";
+			}
+
+			// Password
+			password = cmd.getOptionValue("p");
+
+			if (password == null) {
+				password = "schokouser";
+			}
+
+		} catch (ParseException exp) {
+			// oops, something went wrong
+			System.err.println("Parsing failed.  Reason: " + exp.getMessage());
+
+			// automatically generate the help statement
+			HelpFormatter formatter = new HelpFormatter();
+			formatter.printHelp("ConnectDatabase", options, true);
+		}
+
 		// Datenquelle erzeugen und konfigurieren
 		PGSimpleDataSource ds = new PGSimpleDataSource();
 		// Server IP - Adresse
-		ds.setServerName(ip_adress + ":" + port);
+		ds.setServerName(ip_adress + ":" + port_number);
 		// Datenbank Name
 		ds.setDatabaseName(database);
 		// Username
